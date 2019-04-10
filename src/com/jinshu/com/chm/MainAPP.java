c3e19e21eef6ac165fa56a2ffabf418925b1f77f
hxwjb <hxwjb@users.noreply.github.com>
2019-04-10 20:39:20 +0800

    测试物品

--- src/com/jinshu/com/chm/MainAPP.java
+++ src/com/jinshu/com/chm/MainAPP.java
@@ -7,6 +7,10 @@ public class MainAPP {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         person test1 = new person();
+        item item1 = new item();
+        item1.setName("测试人物");
+
+        System.out.println(item1.getName());
     }
 
 }
