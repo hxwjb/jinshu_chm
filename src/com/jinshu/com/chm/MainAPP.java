7ff6ccbfdaf6eb48f33f315b26895a1e6bb38aa8
hxwjb <hxwjb@users.noreply.github.com>
2019-04-10 20:34:34 +0800

    CHM读取类

---
+++ src/com/jinshu/com/chm/MainAPP.java
@@ -0,0 +1,12 @@
+package com.jinshu.com.chm;
+
+
+public class MainAPP {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        person test1 = new person();
+    }
+
+}
---
+++ src/com/jinshu/com/chm/person.java
@@ -0,0 +1,5 @@
+package com.jinshu.com.chm;
+
+public class person {
+
+}
